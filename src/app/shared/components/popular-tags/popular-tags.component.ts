0a8351538588e540659264563d91a4322fd32555
Lauti <73679349+LautaroBernati@users.noreply.github.com>
2024-04-13 21:00:09 -0300

    fix popular tags in dark theme

--- src/app/shared/components/popular-tags/popular-tags.component.ts
+++ src/app/shared/components/popular-tags/popular-tags.component.ts
@@ -9,20 +9,25 @@ import { ErrorMessageComponent } from '../error-message/error-message.component'
 import { RouterModule } from '@angular/router';
 import { LetModule, PushModule } from '@ngrx/component';
 import { TranslateModule } from '@ngx-translate/core';
+import { ThemesService } from '../../services/themes.service';
 
 @Component({
   selector: 'mc-popular-tags',
   templateUrl: 'popular-tags.component.html',
+  styleUrls: ['popular-tags.component.scss'],
   standalone: true,
   imports: [CommonModule, LoadingComponent, ErrorMessageComponent, RouterModule, PushModule, LetModule, TranslateModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PopularTagsComponent implements OnInit {
   private readonly _store = inject(Store);
+  private readonly _themesService = inject(ThemesService);
+
   public readonly data$ = combineLatest({
     popularTags: this._store.select(selectpopularTagsData),
     isLoading: this._store.select(selectIsLoading),
     error: this._store.select(selectError),
+    theme: this._themesService.appThemes$,
 });
 
   public ngOnInit(): void {
